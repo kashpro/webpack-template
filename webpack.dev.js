3f8f08cae169d6a8a0953f98ceb33beec4ffad55
kashpro <kash083@gmail.com>
2020-12-15 12:20:50 +0800

    15.12.2020 update

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,19 +2,27 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+console.log(process.env.NODE_PUBLIC_PATH);
+console.log(path.join(__dirname, "dist"));
+console.log(path.join(__dirname, process.env.NODE_PUBLIC_PATH));
+
 module.exports = merge(common, {
   /*==============================================================================*/
   mode: "development",
   /*==============================================================================*/
-  output: {
+  // output: {
     // publicPath: "/", // при офлайне "./", для дев-сервера (для несуществующих маршрутов) "/"
-    publicPath: process.env.NODE_PUBLIC_PATH,
-  },
+    // publicPath: process.env.NODE_PUBLIC_PATH,
+    // path: path.resolve(__dirname, process.env.NODE_PUBLIC_PATH),
+  // },
   /*==============================================================================*/
   devtool: "source-map",
   /*==============================================================================*/
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    // publicPath: '/',
+    // contentBase: path.join(__dirname, "dist"),
+    contentBase: "src",
+    watchContentBase: true,
     overlay: true,
     index: 'index.html',
     historyApiFallback: true,
